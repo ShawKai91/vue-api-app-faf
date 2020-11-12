8cca1d84ead849a32614b8db334056236818a1d7
Shaw Kai <shaw.kyle.andrew@gmail.com>
2020-11-11 22:13:48 -0700

    Added search result route and removed sample asset

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
+import SearchResults from '@/components/SearchResults'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HomePage',
       component: HomePage
+    },
+    {
+      path: '/Search-Results',
+      name: 'SearchResults',
+      component: SearchResults
     }
   ]
 })
